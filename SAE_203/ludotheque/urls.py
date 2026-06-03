f9b012df5340791adf72179518c3f7a242c45c77
debenji68 <deiber.benji@gmail.com>
2026-06-03 15:23:37 +0200

    crud

--- SAE_203/ludotheque/urls.py
+++ SAE_203/ludotheque/urls.py
@@ -21,4 +21,12 @@ urlpatterns = [
     path('jeux/ajouter/', views.ajouter_jeu, name='ajouter_jeu'),
     path('jeux/modifier/<int:id>/', views.modifier_jeu, name='modifier_jeu'),
     path('jeux/supprimer/<int:id>/', views.supprimer_jeu, name='supprimer_jeu'),
+
+    path('joueurs/', views.joueur_liste, name='joueur_liste'),
+    path('joueurs/nouveau/', views.joueur_creer, name='joueur_creer'),
+    path('joueurs/<int:pk>/modifier/', views.joueur_modifier, name='joueur_modifier'),
+    path('joueurs/<int:pk>/supprimer/', views.joueur_supprimer, name='joueur_supprimer'),
+    path('joueurs/<int:joueur_id>/liste/', views.ma_liste_jeux, name='ma_liste_jeux'),
+    path('joueurs/<int:joueur_id>/retirer/<int:jeu_id>/', views.retirer_jeu_liste, name='retirer_jeu_liste'),
+    path('jeux/<int:jeu_id>/joueur/<int:joueur_id>/commentaire/', views.commentaire_creer, name='commentaire_creer'),
 ]
--- SAE_203/ludotheque/views.py
+++ SAE_203/ludotheque/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Categorie, Auteur, Jeu
-from .forms import CategorieForm, AuteurForm, JeuForm
+from ludotheque.models import Joueur, Commentaire, ListeJeux# CORRECTION ICI : On importe TOUS les formulaires (les leurs ET les tiens)
+from .forms import CategorieForm, AuteurForm, JeuForm, JoueurForm, CommentaireForm
 
 
 def accueil(request):
     return render(request, 'ludotheque/accueil.html')
 
 
-
 def liste_categories(request):
     categories = Categorie.objects.all()
-
     return render(request, 'ludotheque/categories/liste.html', {
         'categories': categories
     })
@@ -20,13 +19,11 @@ def liste_categories(request):
 def ajouter_categorie(request):
     if request.method == 'POST':
         form = CategorieForm(request.POST)
-
         if form.is_valid():
             form.save()
             return redirect('liste_categories')
     else:
         form = CategorieForm()
-
     return render(request, 'ludotheque/categories/form.html', {
         'form': form,
         'titre': 'Ajouter une catégorie'
@@ -35,16 +32,13 @@ def ajouter_categorie(request):
 
 def modifier_categorie(request, id):
     categorie = get_object_or_404(Categorie, pk=id)
-
     if request.method == 'POST':
         form = CategorieForm(request.POST, instance=categorie)
-
         if form.is_valid():
             form.save()
             return redirect('liste_categories')
     else:
         form = CategorieForm(instance=categorie)
-
     return render(request, 'ludotheque/categories/form.html', {
         'form': form,
         'titre': 'Modifier une catégorie'
@@ -53,20 +47,16 @@ def modifier_categorie(request, id):
 
 def supprimer_categorie(request, id):
     categorie = get_object_or_404(Categorie, pk=id)
-
     if request.method == 'POST':
         categorie.delete()
         return redirect('liste_categories')
-
     return render(request, 'ludotheque/categories/supprimer.html', {
         'categorie': categorie
     })
 
 
-
 def liste_auteurs(request):
     auteurs = Auteur.objects.all()
-
     return render(request, 'ludotheque/auteurs/liste.html', {
         'auteurs': auteurs
     })
@@ -75,13 +65,11 @@ def liste_auteurs(request):
 def ajouter_auteur(request):
     if request.method == 'POST':
         form = AuteurForm(request.POST, request.FILES)
-
         if form.is_valid():
             form.save()
             return redirect('liste_auteurs')
     else:
         form = AuteurForm()
-
     return render(request, 'ludotheque/auteurs/form.html', {
         'form': form,
         'titre': 'Ajouter un auteur'
@@ -90,16 +78,13 @@ def ajouter_auteur(request):
 
 def modifier_auteur(request, id):
     auteur = get_object_or_404(Auteur, pk=id)
-
     if request.method == 'POST':
         form = AuteurForm(request.POST, request.FILES, instance=auteur)
-
         if form.is_valid():
             form.save()
             return redirect('liste_auteurs')
     else:
         form = AuteurForm(instance=auteur)
-
     return render(request, 'ludotheque/auteurs/form.html', {
         'form': form,
         'titre': 'Modifier un auteur'
@@ -108,66 +93,132 @@ def modifier_auteur(request, id):
 
 def supprimer_auteur(request, id):
     auteur = get_object_or_404(Auteur, pk=id)
-
     if request.method == 'POST':
         auteur.delete()
         return redirect('liste_auteurs')
-
     return render(request, 'ludotheque/auteurs/supprimer.html', {
         'auteur': auteur
     })
 
 
-
 def liste_jeux(request):
     jeux = Jeu.objects.all()
-
-    return render(request, 'ludotheque/jeux/liste.html', {
-        'jeux': jeux
-    })
+    return render(request, 'ludotheque/jeux/liste.html', {'jeux': jeux})
 
 
 def ajouter_jeu(request):
     if request.method == 'POST':
         form = JeuForm(request.POST, request.FILES)
-
         if form.is_valid():
             form.save()
             return redirect('liste_jeux')
     else:
         form = JeuForm()
-
-    return render(request, 'ludotheque/jeux/form.html', {
-        'form': form,
-        'titre': 'Ajouter un jeu'
-    })
+    return render(request, 'ludotheque/jeux/form.html', {'form': form,'titre': 'Ajouter un jeu'})
 
 
 def modifier_jeu(request, id):
     jeu = get_object_or_404(Jeu, pk=id)
-
     if request.method == 'POST':
         form = JeuForm(request.POST, request.FILES, instance=jeu)
-
         if form.is_valid():
             form.save()
             return redirect('liste_jeux')
     else:
         form = JeuForm(instance=jeu)
-
-    return render(request, 'ludotheque/jeux/form.html', {
-        'form': form,
-        'titre': 'Modifier un jeu'
-    })
+    return render(request, 'ludotheque/jeux/form.html', {'form': form,'titre': 'Modifier un jeu'})
 
 
 def supprimer_jeu(request, id):
     jeu = get_object_or_404(Jeu, pk=id)
-
     if request.method == 'POST':
         jeu.delete()
         return redirect('liste_jeux')
+    return render(request, 'ludotheque/jeux/supprimer.html', {'jeu': jeu})
 
-    return render(request, 'ludotheque/jeux/supprimer.html', {
-        'jeu': jeu
-    })
+
+def joueur_creer(request):
+    if request.method == "POST":
+        form = JoueurForm(request.POST)
+        if form.is_valid():
+            nouveau_joueur = form.save()
+            ListeJeux.objects.create(joueur=nouveau_joueur)
+            return redirect('joueur_liste')
+    else:
+        form = JoueurForm()
+    return render(request, 'ludotheque/joueur/joueur_form.html', {'form': form, 'action': 'Créer'})
+
+
+def joueur_liste(request):
+    joueurs = Joueur.objects.all()
+    return render(request, 'ludotheque/joueur/joueur_liste.html', {'joueurs': joueurs})
+
+
+def joueur_modifier(request, pk):
+    joueur = get_object_or_404(Joueur, pk=pk)
+    if request.method == "POST":
+        form = JoueurForm(request.POST, instance=joueur)
+        if form.is_valid():
+            form.save()
+            return redirect('joueur_liste')
+    else:
+        form = JoueurForm(instance=joueur)
+    return render(request, 'ludotheque/joueur/joueur_form.html', {'form': form, 'action': 'Modifier'})
+
+
+def joueur_supprimer(request, pk):
+    joueur = get_object_or_404(Joueur, pk=pk)
+    if request.method == "POST":
+        joueur.delete()
+        return redirect('joueur_liste')
+    return render(request, 'ludotheque/joueur/joueur_confirmer_suppression.html', {'joueur': joueur})
+
+
+
+def commentaire_creer(request, jeu_id, joueur_id):
+    jeu = get_object_or_404(Jeu, id=jeu_id)
+    joueur = get_object_or_404(Joueur, id=joueur_id)
+
+    if request.method == "POST":
+        form = CommentaireForm(request.POST)
+        if form.is_valid():
+            commentaire = form.save(commit=False)
+            commentaire.jeu = jeu
+            commentaire.joueur = joueur
+            commentaire.save()
+            return redirect('detail_jeu', pk=jeu.id)
+
+    form = CommentaireForm()
+    return render(request, 'ludotheque/joueur/commentaire_form.html', {'form': form, 'jeu': jeu})
+
+
+def commentaire_supprimer(request, pk):
+    commentaire = get_object_or_404(Commentaire, pk=pk)
+    jeu_id = commentaire.jeu.id
+    commentaire.delete()
+    return redirect('detail_jeu', pk=jeu_id)
+
+
+def ma_liste_jeux(request, joueur_id):
+    joueur = get_object_or_404(Joueur, id=joueur_id)
+    liste_complete = get_object_or_404(ListeJeux, joueur=joueur)
+    jeux_de_la_liste = liste_complete.jeux.all()
+    return render(request, 'ludotheque/joueur/ma_liste.html', {'joueur': joueur,'jeux': jeux_de_la_liste})
+
+
+def ajouter_jeu_liste(request, joueur_id, jeu_id):
+    joueur = get_object_or_404(Joueur, id=joueur_id)
+    jeu = get_object_or_404(Jeu, id=jeu_id)
+    liste_complete = get_object_or_404(ListeJeux, joueur=joueur)
+
+    liste_complete.jeux.add(jeu)
+    return redirect('ma_liste_jeux', joueur_id=joueur.id)
+
+
+def retirer_jeu_liste(request, joueur_id, jeu_id):
+    joueur = get_object_or_404(Joueur, id=joueur_id)
+    jeu = get_object_or_404(Jeu, id=jeu_id)
+    liste_complete = get_object_or_404(ListeJeux, joueur=joueur)
+
+    liste_complete.jeux.remove(jeu)
+    return redirect('ma_liste_jeux', joueur_id=joueur.id)
